e16384123c81f05854bae7c11dc906aa9a499c00
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-11-22 09:02:05 +0100

    copyright

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="footer mt-auto py-3 bg-light">
    <div class="container">
-      <span class="text-muted"></span>
+      <span class="text-muted">Gemaakt door Cho Lommerse en Daniel Garcia Espinales.</span>
    </div>
 </footer>
 
