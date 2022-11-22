1f11b4b00988443c3e03ecaaf64e24a3287fc7ea
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-11-22 08:56:07 +0100

    header en aanpassingen

---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+   <link href="signin.css" rel="stylesheet">
+   <title>Document</title>
+</head>
+<header class="p-3 text-bg-dark">
+   <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+               <use xlink:href="#bootstrap"></use>
+            </svg>
+         </a>
+
+         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="index.php" class="nav-link px-2 text-secondary">Hoofdpagina</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+            <li><a href="faq.php" class="nav-link px-2 text-white">FAQs</a></li>
+            <li><a href="overons.php" class="nav-link px-2 text-white">Over ons</a></li>
+         </ul>
+
+         <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+            <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search">
+         </form>
+
+         <div class="text-end">
+            <button type="button" class="btn btn-outline-light me-2">Inloggen</button>
+            <button type="button" class="btn btn-warning">Registreren</button>
+         </div>
+      </div>
+   </div>
+</header>
+<br>
--- inloggen.php
+++ inloggen.php
@@ -1,49 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-   <link href="signin.css" rel="stylesheet">
-   <title>Document</title>
-</head>
-<header class="p-3 text-bg-dark">
-   <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
-               <use xlink:href="#bootstrap"></use>
-            </svg>
-         </a>
-
-         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="index.php" class="nav-link px-2 text-secondary">Hoofdpagina</a></li>
-            <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
-            <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
-            <li><a href="faq.php" class="nav-link px-2 text-white">FAQs</a></li>
-            <li><a href="overons.php" class="nav-link px-2 text-white">Over ons</a></li>
-         </ul>
-
-         <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-            <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search">
-         </form>
-
-         <div class="text-end">
-            <button type="button" class="btn btn-outline-light me-2">Inloggen</button>
-            <button type="button" class="btn btn-warning">Registreren</button>
-         </div>
-      </div>
-   </div>
-</header>
-<br>
-
 <body class="text-center">
    <main class="form-signin w-100 m-auto">
       <form>
-         <img class="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-         <h1 class="h3 mb-3 fw-normal">Log nu in</h1>
+         <!--<img class="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">-->
+         <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
          <div class="form-floating">
             <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
@@ -53,10 +12,15 @@
             <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
             <label for="floatingPassword">Wachtwoord</label>
          </div>
-         <br>
-         <button class="w-100 btn btn-lg btn-primary" type="submit">Log in</button>
+
+         <div class="checkbox mb-3">
+            <label>
+               <input type="checkbox" value="remember-me"> Remember me
+            </label>
+         </div>
+         <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
          <a href="registreer.php" class="w-100 btn btn-lg btn-warning">Registreer je nu</a>
-         <p class="mt-5 mb-3 text-muted">© 2017–2022</p>
+         <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
       </form>
    </main>
 </body>
