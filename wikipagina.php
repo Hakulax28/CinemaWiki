b16ef973adbba8e2e3dad5b364704070fa33fe06
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-11-22 11:05:02 +0100

    wikipagina

--- wikipagina.php
+++ wikipagina.php
@@ -6,17 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="styleD.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
       <div class="header">1</div>
-      <div class="footer">2</div>
-      <div class="Main">
-        <div class="sidebar">
-            <h1>3</h1>
+      <div class="main">
+        <div>
+            <h1 class="display-3">Title</h1>
+            <h2>subtitle subtitle</h2>
         </div>
-        <div class="MainArticle">4</div>
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque aliquam voluptas ducimus harum tenetur, modi fugiat iste est saepe sapiente tempora consequatur. Asperiores, temporibus voluptatum? Officia, odio eos? Officiis velit quas fuga enim atque vero, et quia illo doloremque eveniet rerum cumque quam aperiam inventore mollitia, dolor debitis est excepturi accusantium nulla nostrum vel quaerat facilis. Odio sequi rem aperiam distinctio libero illo accusantium aspernatur dignissimos odit facilis possimus, exercitationem quos perferendis tempore asperiores optio repellat veritatis consequatur. Voluptatibus numquam enim voluptate nihil fuga neque perspiciatis eius? Corrupti commodi fugit beatae ea, facere dolorum adipisci. Dolore ullam amet libero dignissimos accusantium molestias provident quis fugiat explicabo omnis quos illo assumenda consequuntur quibusdam quisquam architecto obcaecati nihil earum suscipit eius commodi, ut quam dolorem? Qui, expedita quia tenetur eum, ex vel commodi praesentium laudantium error unde quibusdam illum? Incidunt facere expedita quibusdam error voluptates explicabo, voluptate similique reiciendis assumenda sapiente enim dolores corrupti accusamus quae eius eos unde, dolor quas aliquam? Eos perferendis dolor ducimus error eum pariatur maiores itaque, optio, cupiditate veniam voluptates reiciendis culpa asperiores numquam libero corporis excepturi maxime vitae deleniti? Asperiores accusamus, eveniet quasi provident est pariatur rerum, minus optio, illo consectetur eius vero laudantium? Corrupti excepturi, ratione quasi sequi, ipsam nihil quis quae quisquam illo molestias maxime itaque. Veritatis obcaecati odio, enim quia tempore illo minus ab minima vitae facilis perferendis reprehenderit illum inventore earum natus molestias! Debitis ratione saepe veritatis mollitia repudiandae, doloribus nemo dignissimos consequatur cumque praesentium sapiente suscipit aperiam enim tempore nesciunt corrupti quae facere aspernatur veniam officiis facilis explicabo quo sed! Reprehenderit facilis a ea nisi tenetur, dignissimos recusandae accusantium debitis omnis quas vel quae temporibus in quia fuga sapiente eveniet cupiditate voluptatem explicabo. Aut, excepturi consequuntur impedit quisquam mollitia dicta at sapiente et illo illum vero, deserunt enim? Aliquid dolor culpa dolorum doloribus error. Quia nam quos, velit perferendis quam magni iste officia odio tempora, sequi corporis similique, dignissimos voluptate quaerat eos dolor in blanditiis! Placeat dicta repellat commodi quaerat, voluptatum repellendus laudantium nesciunt, pariatur aliquam expedita necessitatibus, reiciendis iusto. Officia accusamus quae cum esse doloribus eius dolore excepturi totam, harum ad asperiores eveniet minus repudiandae perferendis inventore? Corporis nam nihil autem ullam reprehenderit officia blanditiis sequi placeat velit deleniti eius totam, ut unde similique id perspiciatis quo quas dolore aliquid nemo labore maxime minima! Hic nisi molestiae qui veniam dolore voluptates voluptatibus incidunt maiores nesciunt culpa repudiandae ducimus soluta voluptate facilis rem sunt tenetur molestias expedita aspernatur, officia at. Ut, unde veniam ullam nobis sint asperiores autem optio reiciendis quas distinctio, iure laborum consequuntur assumenda vitae reprehenderit, doloribus illo! Facilis accusantium maiores voluptas quia itaque modi quasi a, deleniti saepe sequi necessitatibus veniam quae odio dolorum illo ducimus consequuntur tempora, aliquam eius sit? Odio, voluptatibus? Dolorum amet totam alias illo accusamus aliquam praesentium magni? Tempora modi fugit optio aperiam quod tenetur tempore corrupti placeat, eaque aliquam nostrum quos reiciendis asperiores sit facilis sunt magnam saepe molestiae libero, excepturi iure deleniti consequatur dolorem? Quas, placeat commodi! Ea itaque possimus autem pariatur.</p>
+            <img src="" alt="">
+        </div>
+        
       </div>
+      <div class="sidebar">3</div>
+      <div class="sideimages">b</div>
+      <div class="section1">a</div>
+    <div class="sources">sources</div>
+      <div class="footer">4</div>
     </div>
+    
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
 </html>
