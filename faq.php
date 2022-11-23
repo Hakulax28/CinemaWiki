1aae41d6ada8bde00e16d62a491ecf94295a3cb5
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-11-23 08:58:41 +0100

    aanpassingen

--- faq.php
+++ faq.php
@@ -1,4 +1,15 @@
 <?php include "header.php" ?>
 
+<div>
+   <h1>Facts and Questions</h1>
+   <section class="faq">
+      <h2>Wat doen we?</h2>
+      <p>as</p>
+   </section>
+   <section class="faq">
+      <h2>Welke informatie komt hier op?</h2>
+      <p>as</p>
+   </section>
 
+</div>
 <?php include "footer.php" ?>
--- overons.php
+++ overons.php
@@ -1,4 +1,13 @@
 <?php include "header.php" ?>
 
+<div>
+   <h1>Wat is CinemaWiki?</h1>
+   <section class="about">
+      <p>Wij zijn </p>
+   </section>
+   <section class="about">
+      <p>Ook doen wij</p>
+   </section>
 
+</div>
 <?php include "footer.php" ?>
