87c9c22c2a2c2af8caa4efa60a9e3e4428d3db13
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-11-23 09:45:53 +0100

    aanpassingen aan css enzovoort

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </body>
 
-<footer class="footer mt-auto py-3 bg-light">
+<footer class="footer mt-auto py-3 bg-dark">
    <div class="container">
       <span class="text-muted">Gemaakt door Cho Lommerse en Daniel Garcia Espinales.</span>
    </div>
