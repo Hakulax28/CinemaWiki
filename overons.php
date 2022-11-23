a7501733d2a3582e8a821834ac09b2532ca0e978
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-11-23 11:19:02 +0100

    SVGs toegevoegd

--- overons.php
+++ overons.php
@@ -4,16 +4,16 @@
    <h1>Wat is CinemaWiki?</h1>
    <div class="about">
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt fugiat, libero beatae sit aut animi nisi qui laboriosam numquam amet ducimus alias repellendus et nulla dicta, adipisci nemo nihil pariatur facere! Deserunt consequuntur eum iusto sint. Unde maxime exercitationem, accusantium porro obcaecati veniam maiores fugiat. Quos atque quaerat magnam, a expedita debitis sed, repellat excepturi, animi pariatur sit ea doloribus! Laborum, illum? Rerum quasi ad et officia perferendis corporis autem, voluptates aperiam. Fugiat corrupti, quis iure repellendus placeat repudiandae fuga, quibusdam omnis beatae, unde nihil! Illum fugiat tempora laborum atque aliquam similique eaque ullam, cumque impedit placeat! Architecto qui veritatis quibusdam inventore, tenetur facere sunt maxime, atque quisquam rem magnam quo labore itaque repellat error dolores earum assumenda, excepturi molestiae harum ipsam et. Dolorum maxime vel ipsa quos nostrum blanditiis fugiat reprehenderit velit, at reiciendis officia laudantium voluptas dolore culpa praesentium natus enim. Ducimus explicabo asperiores, id modi iusto aliquid?</p>
-      <img src="test-image.png" height="150px" width="320px" alt=""><br>
+      <img src="images/test-image.png" height="150px" width="320px" alt=""><br>
    </div>
    <br>
    <div class="about">
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima dignissimos nostrum, ipsum, porro officiis rerum voluptates nobis repellat adipisci maiores deleniti ipsam illum quam totam dolore corporis facere a. Quia sint accusamus unde sit rem provident fugiat laborum doloribus, amet, veniam assumenda a aliquid impedit harum nisi saepe consectetur, vel similique iste! Provident, aspernatur! Veniam praesentium sint inventore temporibus enim! Impedit odit culpa expedita at, odio nisi, enim omnis sint vero placeat necessitatibus officia nesciunt quo maiores? Soluta cumque vitae laborum obcaecati exercitationem suscipit quaerat distinctio dolor quod iure? Rem nisi nesciunt saepe eum aliquam est sint iusto distinctio doloribus aut rerum porro dicta accusantium aspernatur necessitatibus harum voluptatibus ullam, nemo similique tempora ratione reiciendis maxime! Aspernatur, dolor voluptas, ducimus iusto harum numquam odio nemo ipsum quis reprehenderit velit nulla! Accusantium est earum quos totam, cupiditate beatae esse, unde possimus, voluptatum quasi saepe laboriosam ipsa sunt tempore molestias. Debitis, ut?</p>
-      <img src="test-image.png" height="150px" width="320px" alt=""><br>
+      <img src="images/test-image.png" height="150px" width="320px" alt=""><br>
    </div>
    <br>
    <div class="about">
-      <img src="test-image.png" height="550px" width="820px" alt="">
+      <img src="images/test-image.png" height="550px" width="820px" alt="">
    </div><br>
 </div>
 <?php include "footer.php" ?>
