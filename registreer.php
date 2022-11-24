18c9bb90da219f686b3bef4d2d0340340207b41e
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-11-24 10:37:04 +0100

    aanpassingen aan de registratie pagina

--- registreer.php
+++ registreer.php
@@ -6,32 +6,41 @@
       <form>
          <!--<img class="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">-->
          <h1 class="h3 mb-3 fw-normal">Registreer je nu in</h1>
-
-         <div class="form-floating">
-            <input type="text" name="firstname" id="floatingInput" class="form-control">
-            <label for="floatingInput">Voornaam: </label>
-         </div><br>
-         <div class="form-floating">
-            <input type="text" name="lastname" id="floatingInput" class="form-control">
-            <label for="floatingInput">Achternaam: </label>
-         </div><br>
-         <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">E-mail</label>
-         </div><br>
-         <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Wachtwoord</label>
-         </div><br>
-         <div class="form-floating">
-            <input type="date" name="date_of_birth" id="floatingInput" class="form-control">
-            <label for="floatingInput">Geboortedatum: </label>
-         </div><br>
-         <div class="form-floating">
-            <input type="tel" name="phonenumber" id="floatingInput" class="form-control">
-            <label for="floatingInput">Telefoonnummer: </label>
-         </div><br>
-
+         <div class="row g-2">
+            <div class="col-md">
+               <div class="form-floating">
+                  <input type="text" name="firstname" id="floatingInput" class="form-control">
+                  <label for="floatingInput">Voornaam: </label>
+               </div><br>
+               <div class="form-floating">
+                  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                  <label for="floatingInput">E-mail</label>
+               </div><br>
+               <div class="form-floating">
+                  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                  <label for="floatingPassword">Wachtwoord</label>
+               </div><br>
+               <div class="form-floating">
+                  <input type="date" name="date_of_birth" id="floatingInput" class="form-control">
+                  <label for="floatingInput">Geboortedatum: </label>
+               </div><br>
+            </div>
+            <div class="col-md">
+               <div class="form-floating">
+                  <input type="text" name="lastname" id="floatingInput" class="form-control">
+                  <label for="floatingInput">Achternaam: </label>
+               </div><br>
+               <div class="form-floating">
+                  <input type="file" id="floatingInput" name="img" accept="image/*" class="form-control"><br>
+                  <input type="submit">
+                  <label for="floatingInput">Select image: </label>
+               </div><br>
+               <div class="form-floating">
+                  <input type="tel" name="phonenumber" id="floatingInput" class="form-control">
+                  <label for="floatingInput">Telefoonnummer: </label>
+               </div><br>
+            </div>
+         </div>
          <button class="w-100 btn btn-lg btn-success" type="submit">registreer je nu in</button>
          <a href="index.php" class="w-100 btn btn-lg btn-danger">Annuleer</a>
          <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
