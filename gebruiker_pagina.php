dab02070b581d9df9f2685e4387cb92205021d5b
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-11-24 11:24:35 +0100

    aanpassingen

--- gebruiker_pagina.php
+++ gebruiker_pagina.php
@@ -1,10 +1,11 @@
 <?php include "header.php" ?>
 
 <div class="container bg-light border border-white rounded-1">
+   <h1>Uw gegevens</h1><br>
    <table class="table table-striped table-dark">
       <thead>
          <tr>
-            <th>ID</th>
+            <!--<th>ID</th>-->
             <th>Voornaam</th>
             <th>Achternaam</th>
             <th>Email</th>
@@ -17,7 +18,7 @@
       </thead>
       <tbody>
          <tr>
-            <td>test</td>
+            <!--<td>test</td>-->
             <td>test</td>
             <td>test</td>
             <td>test</td>
@@ -25,7 +26,7 @@
             <td>test</td>
             <td>test</td>
             <td><a href="delete.php" class="shadow btn btn-danger">Verwijder</a></td>
-            <td><a href="update_gegevens" class="shadow btn btn-warning">Update</a></td>
+            <td><a href="update_gegevens.php" class="shadow btn btn-warning">Update</a></td>
          </tr>
       </tbody>
    </table>
