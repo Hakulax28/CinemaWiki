fe999089fb7f9fce13ba57cfb6a729e2f61211e3
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-11-27 18:20:30 +0100

    footer

--- wikiaanmaken.php
+++ wikiaanmaken.php
@@ -180,10 +180,6 @@ require "connectie.php";
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
-<footer class="footer mt-auto py-3 bg-light">
-   <div class="container">
-      <span class="text-muted">Gemaakt door Cho Lommerse en Daniel Garcia Espinales.</span>
-   </div>
-</footer>
+<?php include "footer.php"; ?>
 
 </html>
