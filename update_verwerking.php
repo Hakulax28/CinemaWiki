73193607efee5859d89909a6d3436c11db679883
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-11-28 10:38:52 +0100

    aanpassing

---
+++ update_verwerking.php
@@ -0,0 +1,46 @@
+<?php
+
+if (isset($_POST["submit"])) {
+   
+   $id = $_GET["user_id"];
+   echo $id;
+
+   if (
+      !empty($_POST["firstname"])
+      || !empty($_POST["lastname"])
+      && !empty($_POST["email"])
+      && !empty($_POST["password"])
+      && !empty($_POST["date_of_birth"])
+      && !empty($_POST["phonenumber"])
+      && !empty($_POST["role"])
+
+   ) {
+
+      //allemaal moeten ze true zijn
+      $firstname = $_POST['firstname'];
+      $lastname = $_POST['lastname'];
+      $email = trim($_POST["email"]);
+      $password = $_POST['password'];
+      $dateofbirth = $_POST['date_of_birth'];
+      $phonenumber = $_POST['phonenumber'];
+      $role = $_POST['role'];
+
+      //database connectie
+      require 'classes/database.php';
+      $sql = "UPDATE users SET
+      firstname = '$firstname',
+      lastname = '$lastname',
+      email = '$email',
+      password = '$password',
+      date_of_birth = '$dateofbirth',
+      phonenumber = '$phonenumber',
+      role = '$role' WHERE user_id = '$id' ";
+
+      if (mysqli_query((new Database())->getConnection(), $sql)) {
+         header("location: user_overzicht.php");
+      }
+
+      echo "Updated successfully";
+      mysqli_close($conn); // Sluit de database verbinding
+   }
+}
