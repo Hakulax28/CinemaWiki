e6d1207cfb81e8bbe91bd4e34a774cbb65e82a47
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-11-29 11:05:29 +0100

    test

--- header.php
+++ header.php
@@ -1,3 +1,12 @@
+<?php
+require 'connectie.php';
+
+session_start();
+
+//if (!$_SESSION["is_logged_in"]) {
+//header("location: inloggen.php");
+//}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -52,7 +61,7 @@
          </form>
 
          <div class="text-end">
-            <?php if ($_SESSION['rol'] == "gebruiker") : ?>
+            <!--<?php if ($_SESSION['rol'] == "gebruiker") : ?>
                <button onclick="document.location='loguit.php'" type="button" class="btn btn-outline-light me-2"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
                      <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                      <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z" />
@@ -71,7 +80,7 @@
                      <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
                      <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z" />
                   </svg> Alle gebruikers</button>
-            <?php endif ?>
+            <?php endif ?>-->
             <button onclick="document.location='inloggen.php'" type="button" class="btn btn-outline-light me-2"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
                   <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                   <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z" />
