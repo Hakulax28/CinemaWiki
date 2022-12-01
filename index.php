69748df7f80b1ce1c8703a962ca9e65273cf2d90
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-12-01 09:01:28 +0100

    aanpassingen

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if ($result = mysqli_query($conn, $sql)) {
    <h2>Featured Film: </h2><br>
    <div class="row g-5">
       <div class="col-md">
-         <img src="images/test-image.png" height="180px" width="320px" alt="">
+         <img src="images/test-image.png" height="220px" width="380px" alt="">
       </div>
       <div class="col-md">
          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus, maxime. Ipsam, earum doloribus aperiam necessitatibus voluptates error quas aliquam labore? Aspernatur quis natus quasi repudiandae doloribus modi dignissimos qui praesentium nobis laboriosam officia veniam similique numquam esse vel dolores at veritatis totam, dolore saepe quia vero illum eius assumenda. Voluptatibus reprehenderit dicta necessitatibus, expedita quae eveniet doloribus blanditiis, quis veniam amet ut minima ipsam possimus? <button onclick="document.location='wikipagina.php'" class="w-100 btn btn-lg btn-success shadow" type="submit">Browse</button></p>
@@ -30,11 +30,11 @@ if ($result = mysqli_query($conn, $sql)) {
    <div class="row g-4 gap-1 mx-auto">
       <?php foreach ($genres as $genre) : ?>
          <div class="card" style="width: 20rem;">
-            <img src="images/test-image.png" class="card-img-top" alt="...">
+            <br><img src="images/test-image.png" class="card-img-top" alt="...">
             <div class="card-body">
                <h5 class="card-title"><?php echo $genre["genreName"] ?></h5>
                <p class="card-text"><?php echo $genre["genreDescription"] ?></p>
-               <a href="#" class="btn btn-primary shadow">Go somewhere</a>
+               <a href="zoeken.php" class="btn btn-primary shadow">Go somewhere</a>
             </div>
          </div>
       <?php endforeach; ?>
