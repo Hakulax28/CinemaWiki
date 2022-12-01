4abea858883880766feef3eb7f6e4b7bd3e8045f
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-12-01 10:31:07 +0100

    terug gebracht

--- wikipagina.php
+++ wikipagina.php
@@ -14,115 +14,113 @@
 <?php include "header.php"; ?>
 
 <body class="bg-secondary bg-gradient">
-  <div class="container">
-    <div class="containerWikipage container">
-      <div class="main">
-        <div class="mainTitle">
-          <h1 class="display-3">Title</h1>
-          <h2>subtitle subtitle</h2>
-        </div>
-        <div class="mainText">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ullam, suscipit totam natus, eveniet molestiae quos inventore quibusdam commodi ratione pariatur! Nesciunt voluptates perferendis obcaecati dolore odio fugit est ratione ullam quaerat at dolor, repellat a nobis? Quaerat nisi veritatis beatae odio aut quos consequuntur voluptas culpa optio quae debitis nihil nam itaque minima similique officia minus error consequatur, odit nulla vero. Minima, explicabo fugiat rerum ea, quod ducimus nesciunt ratione tempora quaerat architecto officia et consequatur iure deserunt totam temporibus neque quidem veritatis perferendis optio id ipsa exercitationem rem. Labore sequi eveniet, dolores repellat accusantium voluptate ipsa tempora magni!</p>
-        </div>
-        <div class="mainImage">
-          <img src="images/test-image.png" class="img-fluid rounded" alt="...">
-        </div>
+  <div class="containerWikipage container">
+    <div class="main">
+      <div class="mainTitle">
+        <h1 class="display-3">Title</h1>
+        <h2>subtitle subtitle</h2>
       </div>
-      <div class="sidebar">
-        <div class="sideImage"><img src="images/test-image.png" alt="" width="125px" height="200px">
-        </div>
-        <div>
-          <h1>title</h1>
-        </div>
-        <div class="sideList">
-          <table class="table">
-            <tbody class="table-group-divider">
-              <tr>
-                <th scope="row">Title</th>
-                <td colspan="3">Film title</td>
-              </tr>
-              <tr>
-                <th scope="row">Runtime</th>
-                <td colspan="3">131 minutes</td>
-              </tr>
-              <tr>
-                <th scope="row">Age Rating</th>
-                <td>EU: 7</td>
-                <td>US: E</td>
-              </tr>
-              <tr>
-                <th scope="row">language</th>
-                <td colspan="3">Dutch</td>
-              </tr>
-              <tr>
-                <th scope="row">Score</th>
-                <td colspan="3">8.7</td>
-              </tr>
-              <tr>
-                <th scope="row">Cost</th>
-                <td colspan="3">8.000.000.000</td>
-              </tr>
-              <tr>
-                <th scope="row">Earnings</th>
-                <td colspan="3">8.000.000.000</td>
-              </tr>
-            </tbody>
-          </table>
-          <table class="table">
-            <thead>
-              <tr>
-                <th>Role</th>
-                <th>Name</th>
-                <th>Age</th>
-              </tr>
-            </thead>
-            <tbody class="table-group-divider">
-              <tr>
-                <th scope="row">Actor</th>
-                <td colspan="2">Bob bob</td>
-                <td>87</td>
-              </tr>
-
-            </tbody>
-          </table>
-        </div>
-        <div class="sideText">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi deserunt impedit delectus libero perferendis qui quos quas beatae earum molestiae aspernatur nemo quasi amet sit, maiores similique aut eaque optio, dolorem in consequuntur. Officia molestias alias tempora perferendis corporis nulla dolorem debitis optio error voluptate? Consequuntur adipisci voluptatibus recusandae deserunt.</p>
-        </div>
+      <div class="mainText">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ullam, suscipit totam natus, eveniet molestiae quos inventore quibusdam commodi ratione pariatur! Nesciunt voluptates perferendis obcaecati dolore odio fugit est ratione ullam quaerat at dolor, repellat a nobis? Quaerat nisi veritatis beatae odio aut quos consequuntur voluptas culpa optio quae debitis nihil nam itaque minima similique officia minus error consequatur, odit nulla vero. Minima, explicabo fugiat rerum ea, quod ducimus nesciunt ratione tempora quaerat architecto officia et consequatur iure deserunt totam temporibus neque quidem veritatis perferendis optio id ipsa exercitationem rem. Labore sequi eveniet, dolores repellat accusantium voluptate ipsa tempora magni!</p>
       </div>
-
-      <div class="sections">
-        <div class="section1Title">
-          <h2>Section 1</h2>
-        </div>
-
-        <div class="section1MainText">
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem, molestiae culpa obcaecati odit aliquam neque iste nostrum dolor nemo quaerat animi blanditiis quasi illum nobis ullam autem minus perferendis non voluptatum provident. Quos velit fuga ab possimus ipsum hic doloribus iusto eligendi explicabo voluptatibus, temporibus vel, tenetur necessitatibus, veniam aut mollitia voluptates magnam at ea nihil quaerat! Dolor voluptates enim ullam quidem recusandae quia quas eligendi praesentium nesciunt explicabo reiciendis asperiores tenetur alias tempora optio molestias nostrum quaerat ratione laudantium in suscipit, autem voluptatibus. Deleniti obcaecati fuga possimus nemo quae dolore nobis veniam mollitia cum, quasi asperiores aperiam aliquid. Harum. ipsum dolor sit amet consectetur adipisicing elit. Nemo, eaque porro! Laborum beatae, sequi magni iste fugit non provident facilis repellendus quia culpa eius incidunt autem saepe? Vitae libero tempora, fugiat veritatis deleniti minima, perspiciatis incidunt animi totam temporibus facilis est. Ea id omnis porro dolores iusto vero asperiores itaque.</p>
-        </div>
-
-        <div class="section1Image"><img src="images/test-image.png" alt="" class="img-fluid rounded"></div>
-
-        <div class="section1ExtraText">
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur voluptatum veniam quibusdam neque, praesentium architecto error cumque eaque deleniti exercitationem veritatis tempore iste laborum minus? Tempore omnis, commodi tempora ex pariatur temporibus voluptatum et dolorem deleniti qui magnam debitis veritatis, accusantium perferendis cupiditate sit quaerat voluptatibus itaque facilis rem ea culpa eveniet! Officia nulla repellendus, beatae quidem molestiae ab est quaerat ut temporibus cumque ipsum sunt, voluptas laboriosam eos impedit sit sapiente quas culpa recusandae vero assumenda nemo. Quisquam natus, repellat suscipit eius aliquid sequi exercitationem rerum aperiam reiciendis optio deserunt, ex dolor, debitis porro minima id distinctio vero dicta!</p>
-        </div>
-
-        <div class="section2Title">
-          <h2>section 2</h2>
-        </div>
-
-        <div class="section2MainText">
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis porro vero perspiciatis dicta fuga minima? Ducimus, quas placeat. Obcaecati quibusdam provident aperiam sapiente, numquam temporibus consectetur, inventore ratione ipsum omnis fugiat pariatur, cupiditate laudantium. Et assumenda aliquam accusamus repudiandae culpa, nostrum nihil pariatur quidem ipsum, cum dignissimos maiores tempore aut nam quia tenetur nulla consequatur dolorum quam eius aliquid in mollitia impedit veritatis! Nisi vitae maiores amet sapiente perferendis, impedit temporibus inventore pariatur omnis veritatis aut fuga labore architecto facilis tempora magnam doloremque nemo quod. Suscipit mollitia quaerat dolores maiores pariatur magni aliquam reprehenderit voluptatem quia, illum natus fugiat debitis ratione assumenda magnam deserunt veniam dolor asperiores quis, tempore inventore numquam ipsa delectus. Blanditiis inventore sit odio dolorem magni eos illo quod dolorum accusantium, animi at beatae cupiditate, consectetur sunt eligendi hic facere. Modi consequatur voluptate tempora maiores molestiae nostrum ut, corporis dignissimos voluptates laudantium soluta vel quasi deleniti repellendus repellat delectus animi consectetur expedita aliquam porro quo minima maxime est eveniet. Vitae porro fugiat, enim exercitationem accusamus, aut excepturi quasi, inventore deserunt earum hic non? Deserunt sapiente voluptas quis. Quia id culpa a asperiores, neque enim provident? Praesentium pariatur nostrum esse consequatur expedita quo dolores iste, nihil voluptas dignissimos!
-          </p>
-        </div>
+      <div class="mainImage">
+        <img src="images/test-image.png" class="img-fluid rounded" alt="...">
       </div>
-      <div class="sideimages"><img src="images/test-image.png" alt="" class="img-fluid rounded">
-        <img src="images/test-image.png" alt="" class="img-fluid rounded">
+    </div>
+    <div class="sidebar">
+      <div class="sideImage"><img src="images/test-image.png" alt="" width="125px" height="200px">
       </div>
-      <div class="sources">sources</div>
+      <div>
+        <h1>title</h1>
+      </div>
+      <div class="sideList">
+        <table class="table">
+          <tbody class="table-group-divider">
+            <tr>
+              <th scope="row">Title</th>
+              <td colspan="3">Film title</td>
+            </tr>
+            <tr>
+              <th scope="row">Runtime</th>
+              <td colspan="3">131 minutes</td>
+            </tr>
+            <tr>
+              <th scope="row">Age Rating</th>
+              <td>EU: 7</td>
+              <td>US: E</td>
+            </tr>
+            <tr>
+              <th scope="row">language</th>
+              <td colspan="3">Dutch</td>
+            </tr>
+            <tr>
+              <th scope="row">Score</th>
+              <td colspan="3">8.7</td>
+            </tr>
+            <tr>
+              <th scope="row">Cost</th>
+              <td colspan="3">8.000.000.000</td>
+            </tr>
+            <tr>
+              <th scope="row">Earnings</th>
+              <td colspan="3">8.000.000.000</td>
+            </tr>
+          </tbody>
+        </table>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Role</th>
+              <th>Name</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody class="table-group-divider">
+            <tr>
+              <th scope="row">Actor</th>
+              <td colspan="2">Bob bob</td>
+              <td>87</td>
+            </tr>
 
+          </tbody>
+        </table>
+      </div>
+      <div class="sideText">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi deserunt impedit delectus libero perferendis qui quos quas beatae earum molestiae aspernatur nemo quasi amet sit, maiores similique aut eaque optio, dolorem in consequuntur. Officia molestias alias tempora perferendis corporis nulla dolorem debitis optio error voluptate? Consequuntur adipisci voluptatibus recusandae deserunt.</p>
+      </div>
     </div>
 
+    <div class="sections">
+      <div class="section1Title">
+        <h2>Section 1</h2>
+      </div>
+
+      <div class="section1MainText">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem, molestiae culpa obcaecati odit aliquam neque iste nostrum dolor nemo quaerat animi blanditiis quasi illum nobis ullam autem minus perferendis non voluptatum provident. Quos velit fuga ab possimus ipsum hic doloribus iusto eligendi explicabo voluptatibus, temporibus vel, tenetur necessitatibus, veniam aut mollitia voluptates magnam at ea nihil quaerat! Dolor voluptates enim ullam quidem recusandae quia quas eligendi praesentium nesciunt explicabo reiciendis asperiores tenetur alias tempora optio molestias nostrum quaerat ratione laudantium in suscipit, autem voluptatibus. Deleniti obcaecati fuga possimus nemo quae dolore nobis veniam mollitia cum, quasi asperiores aperiam aliquid. Harum. ipsum dolor sit amet consectetur adipisicing elit. Nemo, eaque porro! Laborum beatae, sequi magni iste fugit non provident facilis repellendus quia culpa eius incidunt autem saepe? Vitae libero tempora, fugiat veritatis deleniti minima, perspiciatis incidunt animi totam temporibus facilis est. Ea id omnis porro dolores iusto vero asperiores itaque.</p>
+      </div>
+
+      <div class="section1Image"><img src="images/test-image.png" alt="" class="img-fluid rounded"></div>
+
+      <div class="section1ExtraText">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur voluptatum veniam quibusdam neque, praesentium architecto error cumque eaque deleniti exercitationem veritatis tempore iste laborum minus? Tempore omnis, commodi tempora ex pariatur temporibus voluptatum et dolorem deleniti qui magnam debitis veritatis, accusantium perferendis cupiditate sit quaerat voluptatibus itaque facilis rem ea culpa eveniet! Officia nulla repellendus, beatae quidem molestiae ab est quaerat ut temporibus cumque ipsum sunt, voluptas laboriosam eos impedit sit sapiente quas culpa recusandae vero assumenda nemo. Quisquam natus, repellat suscipit eius aliquid sequi exercitationem rerum aperiam reiciendis optio deserunt, ex dolor, debitis porro minima id distinctio vero dicta!</p>
+      </div>
+
+      <div class="section2Title">
+        <h2>section 2</h2>
+      </div>
+
+      <div class="section2MainText">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis porro vero perspiciatis dicta fuga minima? Ducimus, quas placeat. Obcaecati quibusdam provident aperiam sapiente, numquam temporibus consectetur, inventore ratione ipsum omnis fugiat pariatur, cupiditate laudantium. Et assumenda aliquam accusamus repudiandae culpa, nostrum nihil pariatur quidem ipsum, cum dignissimos maiores tempore aut nam quia tenetur nulla consequatur dolorum quam eius aliquid in mollitia impedit veritatis! Nisi vitae maiores amet sapiente perferendis, impedit temporibus inventore pariatur omnis veritatis aut fuga labore architecto facilis tempora magnam doloremque nemo quod. Suscipit mollitia quaerat dolores maiores pariatur magni aliquam reprehenderit voluptatem quia, illum natus fugiat debitis ratione assumenda magnam deserunt veniam dolor asperiores quis, tempore inventore numquam ipsa delectus. Blanditiis inventore sit odio dolorem magni eos illo quod dolorum accusantium, animi at beatae cupiditate, consectetur sunt eligendi hic facere. Modi consequatur voluptate tempora maiores molestiae nostrum ut, corporis dignissimos voluptates laudantium soluta vel quasi deleniti repellendus repellat delectus animi consectetur expedita aliquam porro quo minima maxime est eveniet. Vitae porro fugiat, enim exercitationem accusamus, aut excepturi quasi, inventore deserunt earum hic non? Deserunt sapiente voluptas quis. Quia id culpa a asperiores, neque enim provident? Praesentium pariatur nostrum esse consequatur expedita quo dolores iste, nihil voluptas dignissimos!
+        </p>
+      </div>
+    </div>
+    <div class="sideimages"><img src="images/test-image.png" alt="" class="img-fluid rounded">
+      <img src="images/test-image.png" alt="" class="img-fluid rounded">
+    </div>
+    <div class="sources">sources</div>
+
   </div>
+
 </body>
 <?php include "footer.php"; ?>
 
