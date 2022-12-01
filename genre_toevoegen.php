c52436cce5dca35197479e509579ac902bfe4cfd
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-12-01 08:43:22 +0100

    aanpassingen

--- genre_toevoegen.php
+++ genre_toevoegen.php
@@ -21,28 +21,16 @@ VALUES ('$genreName','$genreDesc')";
     echo "<script>alert('Vul alle velden in!!!')</script>";
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="styleD.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
-</head>
 
 <?php include "header.php"; ?>
 <div class="container bg-light border border-white rounded-1">
-    <h2>Genre toevoegen</h2>
+    <h1>Genre toevoegen</h1><br>
     <form action="" method="POST">
-        <input class="form-control" type="text" name="genreName" placeholder="Naam" aria-label="name">
-        <input class="form-control" type="text" name="genreDescription" placeholder="Descriptie" aria-label="age">
+        <input class="form-control" type="text" name="genreName" placeholder="Naam" aria-label="name"><br>
+        <input class="form-control" type="text" name="genreDescription" placeholder="Descriptie" aria-label="age"><br>
         <button class="btn btn-primary" name="submit" type="submit">Voeg toe</button>
         <button class="btn btn-danger" onclick="history.back()">Ga terug</button>
-    </form>
+    </form><br>
 </div>
 
 <?php include "footer.php"; ?>
