d537ea8ab39825b703dcd92dd8f6f50125dffea0
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-12-01 13:36:25 +0100

    aanpassingen

--- overons.php
+++ overons.php
@@ -7,12 +7,12 @@
          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae dolore temporibus, obcaecati quam illo nam non repellendus mollitia. Et dolor optio aut unde officia maiores incidunt facere quas? Culpa voluptatum ipsam eveniet magnam? Sunt velit incidunt laboriosam, eius cumque molestias voluptates mollitia necessitatibus expedita corrupti! At cupiditate odio consequuntur soluta deleniti ad, quibusdam quos in modi quas ipsam nemo voluptas expedita reiciendis nulla placeat pariatur ipsum repellendus! Unde animi voluptas quod, amet veniam vitae nesciunt mollitia illum, sequi, facilis voluptates!</p>
       </div>
       <div class="col-md">
-         <img src="images/test-image.png" height="180px" width="320px" alt="">
+         <img src="images/test-image.png" class="foto" alt="">
       </div>
    </div><br>
    <div class="row g-5">
       <div class="col-md">
-         <img src="images/test-image.png" height="180px" width="320px" alt="">
+         <img src="images/test-image.png" class="foto" alt="">
       </div>
       <div class="col-md">
          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae dolore temporibus, obcaecati quam illo nam non repellendus mollitia. Et dolor optio aut unde officia maiores incidunt facere quas? Culpa voluptatum ipsam eveniet magnam? Sunt velit incidunt laboriosam, eius cumque molestias voluptates mollitia necessitatibus expedita corrupti! At cupiditate odio consequuntur soluta deleniti ad, quibusdam quos in modi quas ipsam nemo voluptas expedita reiciendis nulla placeat pariatur ipsum repellendus! Unde animi voluptas quod, amet veniam vitae nesciunt mollitia illum, sequi, facilis voluptates!</p>
