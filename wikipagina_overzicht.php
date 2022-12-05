e4e7fe8a0019a0df7a9c32062c259f469786f2ff
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-12-05 09:13:41 +0100

    pagina toegevoegd

---
+++ wikipagina_overzicht.php
@@ -0,0 +1,49 @@
+<?php require 'connectie.php';
+
+// hier moet de info van de anderen tabelen te voor schijn komen. 
+
+$sql = "SELECT * FROM users";
+
+if ($result = mysqli_query($conn, $sql)) {
+   $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+
+?>
+<?php include "header.php" ?>
+
+<div class="container bg-light border border-white rounded-1"><br>
+   <h1>Uw gegevens</h1><br>
+   <table class="table table-striped table-dark">
+      <thead>
+         <tr>
+            <!--<th>ID</th>-->
+            <th>Voornaam</th>
+            <th>Achternaam</th>
+            <th>Email</th>
+            <th>Wachtwoord</th>
+            <th>Geboortedatum</th>
+            <th>Telefoonnummer</th>
+            <th>Rol</th>
+            <th>Verwijder</th>
+            <th>Update</th>
+         </tr>
+      </thead>
+      <tbody>
+         <?php foreach ($users as $user) : ?>
+            <tr>
+               <!--<td><?php echo $user["user_id"] ?></td>-->
+               <td><?php echo $user["firstname"] ?></td>
+               <td><?php echo $user["lastname"] ?></td>
+               <td><?php echo $user["email"] ?></td>
+               <td><?php echo $user["password"] ?></td>
+               <td><?php echo $user["date_of_birth"] ?></td>
+               <td><?php echo $user["phonenumber"] ?></td>
+               <td><?php echo $user["role"] ?></td>
+               <td><a href="gebruiker_delete.php?user_id=<?php echo $user["user_id"] ?>" class="shadow btn btn-danger shadow">Verwijder</a></td>
+               <td><a href="gebruiker_update.php?user_id=<?php echo $user["user_id"] ?>" class="shadow btn btn-warning shadow">Update</a></td>
+            </tr>
+         <?php endforeach; ?>
+      </tbody>
+   </table>
+</div>
+<?php include "footer.php" ?>
