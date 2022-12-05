5efee0e2b4598d432f37f22cbc56c993c9366b8d
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-12-05 09:11:47 +0100

    pagina aangemaakt

---
+++ wikipagina_delete.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+print_r($_GET["page_id"]);
+
+require 'connectie.php';
+
+$id = $_GET['page_id'];
+
+$sql = "DELETE FROM wikipages WHERE page_id = $id";
+
+if (mysqli_query($conn, $sql)) {
+   header("location: wikipagina.php");
+}
