13deaa83304d09b3e5eec42a34ea363026a7b9c7
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-12-06 09:17:04 +0100

    aanpassingen

--- geschiedenis.php
+++ geschiedenis.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="styleD.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    
-</head>
 
 <?php include "header.php"; ?>
 
@@ -17,26 +6,27 @@
         <h2 class="display-1">Bewerkingen geschiedenis</h2>
         <div class="d-flex flex-column mb-3 mt-3 border bg-light shadow-sm bg-gradient rounded">
             <div class="p-2 col justify-content-start d-flex align-items-center">
-               <img src="images/test-image.png" alt="" width="100px" height="100px" class="rounded">
-               <h2 class="p-2">(Username) has edited (Page) with (Text)</h2>
-            </div>
-        </div>
-    
-        <div class="d-flex flex-column mb-3 mt-3 border bg-light shadow-sm bg-gradient rounded">
-            <div class="p-2 col justify-content-start d-flex align-items-center">
-               <img src="images/test-image.png" alt="" width="100px" height="100px" class="rounded">
-               <h2 class="p-2">(Username) has edited (Page) with (Text)</h2>
+                <img src="images/test-image.png" alt="" width="100px" height="100px" class="rounded">
+                <h2 class="p-2">(Username) has edited (Page) with (Text)</h2>
             </div>
         </div>
 
         <div class="d-flex flex-column mb-3 mt-3 border bg-light shadow-sm bg-gradient rounded">
             <div class="p-2 col justify-content-start d-flex align-items-center">
-               <img src="images/test-image.png" alt="" width="100px" height="100px" class="rounded">
-               <h2 class="p-2">(Username) has edited (Page) with (Text)</h2>
+                <img src="images/test-image.png" alt="" width="100px" height="100px" class="rounded">
+                <h2 class="p-2">(Username) has edited (Page) with (Text)</h2>
             </div>
         </div>
 
+        <div class="d-flex flex-column mb-3 mt-3 border bg-light shadow-sm bg-gradient rounded">
+            <div class="p-2 col justify-content-start d-flex align-items-center">
+                <img src="images/test-image.png" alt="" width="100px" height="100px" class="rounded">
+                <h2 class="p-2">(Username) has edited (Page) with (Text)</h2>
+            </div>
+        </div>
+        <a href="wikipagina.php?page_id=1" class="w-100 btn btn-lg btn-success shadow" type="submit">Ga hier terug</a>
     </div>
+
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
