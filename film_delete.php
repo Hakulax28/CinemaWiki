8678f0a4874e16b04588ed30d2ea5eaa7d036233
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-12-08 11:28:48 +0100

    bestand toegevoegd

--- film_delete.php
+++ film_delete.php
@@ -2,14 +2,14 @@
 
 session_start();
 
-print_r($_GET["user_id"]);
+print_r($_GET["film_id"]);
 
 require 'connectie.php';
 
-$id = $_GET['user_id'];
+$id = $_GET['film_id'];
 
-$sql = "DELETE FROM users WHERE user_id = $id";
+$sql = "DELETE FROM films WHERE film_id = $id";
 
 if (mysqli_query($conn, $sql)) {
-   header("location: loguit.php");
+   header("location: film_overzicht.php");
 }
