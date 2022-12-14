f79530368e09730a508ca7ea8696bb5da5113d38
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-12-14 09:03:11 +0100

    wachtwoord verificatie werkt

--- process_login.php
+++ process_login.php
@@ -4,45 +4,50 @@ require 'connectie.php';
 
 session_start();
 
-$_SESSION["temp_data"]["message"] = null;
+
 
 if (empty($_POST["email"]) && empty($_POST["password"])) {
    header("location: inloggen.php");
 }
 
-$id = $_POST["user_id"];
-$email = $_POST["email"];
-$password = $_POST["password"];
+if (isset($_POST["email"]) && isset($_POST["password"])) {
+   
+   
+   $email = $_POST["email"];
+   $password = $_POST["password"];
+   
+   $sql = "SELECT * FROM users WHERE email = '$email' ";
+   
+   $result = mysqli_query($conn, $sql);
+   //var_dump(mysqli_num_rows($result));die;
+   
+   if (mysqli_num_rows($result) > 0) {
+      
+     
+      $user = mysqli_fetch_assoc($result);
 
-$sql = "SELECT * FROM users WHERE email = '$email' ";
+      if ($user['password']  === $password) {
+         //hier kent het de gebruiker en het wachtwoord
+         $_SESSION["user_id"] = $user["user_id"];
+         $_SESSION["email"] = $user["email"];
+         $_SESSION["is_logged_in"] = true;
+         $_SESSION["role"] = $user["role"];
 
-$result = mysqli_query($conn, $sql);
-//var_dump(mysqli_num_rows($result));die;
-
-if ($result) {
-   $user = mysqli_fetch_assoc($result);
-
-   if ($_POST["password"] === $password && $_POST["email"] === $email) {
-      //hier kent het de gebruiker
-      $_SESSION["user_id"] = $user["user_id"];
-      $_SESSION["email"] = $user["email"];
-      $_SESSION["is_logged_in"] = true;
-      $_SESSION["role"] = $user["role"];
-
-      var_dump($_SESSION);
-      //Hier bekijkt hij of degene die heeft ingelogd een klant of medewerker is.
-      if ($_SESSION["role"] == "beheerder") {
-         echo "U kan nu alles doen";
-         header("location: index.php");
-      } else if ($_SESSION["role"] == "gebruiker") {
-         echo "U kan alleen de website zelf bekijken";
-         header("location: index.php");
-      }
-   } else if (is_null($user)) {
-      //gebruiker onbekend
-      header("location: inloggen.php");
-      echo "Gebruiker_Onbekend";
-      var_dump($user);
-      die;
+   
+         //Hier bekijkt hij of degene die heeft ingelogd een klant of medewerker is.
+         if ($_SESSION["role"] == "beheerder") {
+            echo "U kan nu alles doen";
+            header("location: index.php");
+            exit;
+         } else if ($_SESSION["role"] == "gebruiker") {
+            echo "U kan alleen de website zelf bekijken";
+            header("location: index.php");
+            exit;
+         }
+      } 
+      
    }
+
+   $_SESSION['error_message'] = "Deze gegevens zijn onbekend!";
+   header("location: inloggen.php");
 }
