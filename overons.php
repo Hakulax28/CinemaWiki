f9e164a6aeab68a54003b6aed58662fd0346c987
Daniel Garcia <d.a.garcia.espinales@gmail.com>
2022-12-14 11:37:22 +0100

    over ons

--- overons.php
+++ overons.php
@@ -4,23 +4,23 @@
    <h1>Wat is CinemaWiki?</h1><br>
    <div class="row g-5">
       <div class="col-md">
-         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae dolore temporibus, obcaecati quam illo nam non repellendus mollitia. Et dolor optio aut unde officia maiores incidunt facere quas? Culpa voluptatum ipsam eveniet magnam? Sunt velit incidunt laboriosam, eius cumque molestias voluptates mollitia necessitatibus expedita corrupti! At cupiditate odio consequuntur soluta deleniti ad, quibusdam quos in modi quas ipsam nemo voluptas expedita reiciendis nulla placeat pariatur ipsum repellendus! Unde animi voluptas quod, amet veniam vitae nesciunt mollitia illum, sequi, facilis voluptates!</p>
+         <p>CinemaWiki is een online platform dat is opgericht door een groep filmliefhebbers die gepassioneerd zijn over het delen van hun kennis over films en filmgeschiedenis. Onze missie is om een ruimte te creëren waar iedereen kan bijdragen en leren over de wereld van cinema.</p>
       </div>
       <div class="col-md">
-         <img src="images/test-image.png" class="foto" alt="">
+         <img src="images/filmscenes/ca-times.brightspotcdn.jpg" class="foto" alt="">
       </div>
    </div><br>
    <div class="row g-5">
       <div class="col-md">
-         <img src="images/test-image.png" class="foto" alt="">
+         <img src="images/filmscenes/13a6804976e460dfdfb450acc1d22e8e.jpeg" class="foto" alt="">
       </div>
       <div class="col-md">
-         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae dolore temporibus, obcaecati quam illo nam non repellendus mollitia. Et dolor optio aut unde officia maiores incidunt facere quas? Culpa voluptatum ipsam eveniet magnam? Sunt velit incidunt laboriosam, eius cumque molestias voluptates mollitia necessitatibus expedita corrupti! At cupiditate odio consequuntur soluta deleniti ad, quibusdam quos in modi quas ipsam nemo voluptas expedita reiciendis nulla placeat pariatur ipsum repellendus! Unde animi voluptas quod, amet veniam vitae nesciunt mollitia illum, sequi, facilis voluptates!</p>
+         <p>Op CinemaWiki kun je lezen over klassieke en moderne films, achtergrondinformatie over filmmakers en acteurs, en alles wat je wilt weten over de geschiedenis van de filmindustrie. We zijn constant bezig om onze inhoud te verbeteren en te updaten, dus blijf ons volgen om op de hoogte te blijven van de laatste ontwikkelingen op het gebied van cinema.</p>
       </div>
    </div>
    <br>
    <div>
-      <img src="images/test-image.png" class="foto2" alt="">
+      <img src="images/filmscenes/Fight-Club-The-Final-Scene.webp" class="foto2" alt="">
    </div><br>
 </div>
 <?php include "footer.php" ?>
