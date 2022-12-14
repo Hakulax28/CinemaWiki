a65323cfbe7713952baccfa6c07d79d63874164c
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-12-14 09:46:08 +0100

    aanpassingen

--- process_login.php
+++ process_login.php
@@ -4,26 +4,23 @@ require 'connectie.php';
 
 session_start();
 
-
-
 if (empty($_POST["email"]) && empty($_POST["password"])) {
    header("location: inloggen.php");
 }
 
 if (isset($_POST["email"]) && isset($_POST["password"])) {
-   
-   
+
+
    $email = $_POST["email"];
    $password = $_POST["password"];
-   
+
    $sql = "SELECT * FROM users WHERE email = '$email' ";
-   
+
    $result = mysqli_query($conn, $sql);
    //var_dump(mysqli_num_rows($result));die;
-   
+
    if (mysqli_num_rows($result) > 0) {
-      
-     
+
       $user = mysqli_fetch_assoc($result);
 
       if ($user['password']  === $password) {
@@ -33,7 +30,6 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
          $_SESSION["is_logged_in"] = true;
          $_SESSION["role"] = $user["role"];
 
-   
          //Hier bekijkt hij of degene die heeft ingelogd een klant of medewerker is.
          if ($_SESSION["role"] == "beheerder") {
             echo "U kan nu alles doen";
@@ -44,8 +40,7 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
             header("location: index.php");
             exit;
          }
-      } 
-      
+      }
    }
 
    $_SESSION['error_message'] = "Deze gegevens zijn onbekend!";
