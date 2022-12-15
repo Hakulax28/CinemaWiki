ff8a0aa3aa09774ef3b683a5bdc7780cc461b9ee
Cho Lommerse <cho.lommerse@ziggo.nl>
2022-12-15 09:17:32 +0100

    aanpassingen

--- persoon.php
+++ persoon.php
@@ -2,10 +2,10 @@
 
 // hier moet de info van de anderen tabelen te voor schijn komen. 
 
-$sql = "SELECT * FROM genre";
+$sql = "SELECT * FROM people";
 
 if ($result = mysqli_query($conn, $sql)) {
-   $genres = mysqli_fetch_all($result, MYSQLI_ASSOC);
+   $persons = mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
 
 ?>
@@ -17,7 +17,10 @@ if ($result = mysqli_query($conn, $sql)) {
       <thead>
          <tr>
             <!--<th>ID</th>-->
-            <th>Genre naam</th>
+            <th>Persoons naam</th>
+            <th>Leeftijd</th>
+            <th>De rol</th>
+            <th>Foto</th>
             <th>Descriptie</th>
             <?php if (!empty($_SESSION)) : ?>
                <?php if ($_SESSION['role'] == "beheerder") : ?>
@@ -28,17 +31,36 @@ if ($result = mysqli_query($conn, $sql)) {
          </tr>
       </thead>
       <tbody>
-         <?php foreach ($genres as $genre) : ?>
+         <?php foreach ($persons as $person) : ?>
             <tr>
-               <!--<td><?php echo $genre["genre_id"] ?></td>-->
-               <td><?php echo $genre["genreName"] ?></td>
-               <td><?php echo $genre["genreDescription"] ?></td>
-               <?php if (!empty($_SESSION)) : ?>
-                  <?php if ($_SESSION['role'] == "beheerder") : ?>
-                     <td><a href="genre_delete.php?genre_id=<?php echo $genre["genre_id"] ?>" class="shadow btn btn-danger shadow">Verwijder</a></td>
-                     <td><a href="genre_update.php?genre_id=<?php echo $genre["genre_id"] ?>" class="shadow btn btn-warning shadow">Update</a></td>
+               <!--<td><?php echo $person["genre_id"] ?></td>-->
+               <td><?php echo $person["personName"] ?></td>
+               <td><?php echo $person["personAge"] ?></td>
+               <td><?php echo $person["personRole"] ?></td>
+               <td><img src="<?php echo $person["personImage"] ?>" alt="" width="90px" height="100px"></td>
+               <td><button type="button" class="btn btn-primary" style="width: 150px;" data-bs-toggle="modal" data-bs-target="#ModalSection1Text1<?php echo $person["person_id"] ?>">Bekijk hun descriptie</button></td>
+
+               <div class="modal fade" id="ModalSection1Text1<?php echo $person["person_id"] ?>" tabindex="-1" aria-labelledby="ModalSection1Text1<?php echo $person["person_id"] ?>Label" aria-hidden="true">
+                  <div class="modal-dialog">
+                     <div class="modal-content">
+                        <div class="modal-header">
+                           <h1 class="modal-title fs-5" id="ModalSection1Text1<?php echo $person["person_id"] ?>Label">Descriptie</h1>
+                           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                           <?php echo $person["personDescription"] ?>
+                        </div>
+                        <div class="modal-footer">
+                           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                     </div>
+                  </div>
+                  <?php if (!empty($_SESSION)) : ?>
+                     <?php if ($_SESSION['role'] == "beheerder") : ?>
+                        <td><a href="persoon_bewerken.php?person_id=<?php echo $person["person_id"] ?>" class="shadow btn btn-danger shadow">Verwijder</a></td>
+                        <td><a href="persoon_bewerken.php?person_id=<?php echo $person["person_id"] ?>" class="shadow btn btn-warning shadow">Update</a></td>
+                     <?php endif ?>
                   <?php endif ?>
-               <?php endif ?>
             </tr>
          <?php endforeach; ?>
       </tbody>
